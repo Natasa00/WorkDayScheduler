e23dfb581c24694a1d81111761764eb14f2bc130
Natasa <nkilibarda3@gmail.com>
2023-04-15 21:19:09 +1000

    Display current date using jQuery and day.js

--- Develop/script.js
+++ Develop/script.js
@@ -21,3 +21,6 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var currentDayEl = $("#currentDay");
+currentDayEl.text(dayjs().format("dddd, MMMM D"));
